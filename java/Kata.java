3628cc98bc6fb2cd733b4718a15fbd5348820e6e
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-04-02 20:45:30 +0200

    message

---
+++ java/Kata.java
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/java
+// FUNDAMENTALS
+
+public class Kata {
+    public static void main(String[] args) {
+        System.out.println(quarterOf(5));
+       } 
+    public static int quarterOf(int month) {
+        return (month+2)/3;
+    }
+}
