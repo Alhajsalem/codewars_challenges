f58777c90e7be794b635231d1560ac312da5bac4
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-04-04 13:25:59 +0200

    rgex

---
+++ python/6kyu_rgex_simple_sentences.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/5297bf69649be865e6000922/train/python
+# FUNDAMENTALS STRINGS REGULAR EXPRESSIONS
+import re
+def make_sentences(parts):
+    return (re.sub('\s[,]', ",", re.sub('\s[.]', '', " ".join(parts)))+".")
+
+assert make_sentences(['The', 'Earth', 'rotates', 'around', 'The', 'Sun', 'in', '365', 'days', ',', 'I', 'know', 'that', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']) == 'The Earth rotates around The Sun in 365 days, I know that.'
+
+
+
+# better solution
+make_sentences = lambda c:re.sub(r'(?<=\w) (?=\W)|[.\s]+$','',' '.join(c))+'.'
+
+# better solution
+def make_sentences(parts):
+    return ' '.join(parts).replace(' ,', ',').strip(' .') + '.'
