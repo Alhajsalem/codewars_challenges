404891243844d782be7e85b45612848afe9ec417
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-04-05 20:13:24 +0200

    message

---
+++ javascript/6kyu_valid_string.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/52f3bb2095d6bfeac2002196/train/javascript 
+
+var validWord = function(valuesArray, word) {
+    if (sortAlphabet(word) ==  sortAlphabet(valuesArray.join(""))){
+        return true
+    } 
+    else {
+        return false
+    } 
+}
+function sortAlphabet(str) {
+    return [...str].sort((a, b) => a.localeCompare(b)).join("");
+  }
+
+validWord(['code','wars'], 'CodeStarsWar')
