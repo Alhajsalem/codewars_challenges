2d5c5bcb4fec62f1cdee49c56230112dd1acfd70
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-04-21 07:44:19 +0200

    message

--- python/6kyu_stop_gnininpS_My.py
+++ python/6kyu_stop_gnininpS_My.py
@@ -11,3 +11,12 @@ def spin_words(sentence):
     return " ".join(result)
     
 spin_words("Hey fellow warriors")
+
+
+# https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
+
+def high_and_low(numbers):
+    return "{} {}".format(str(max([int(i) for i in numbers.split(" ")])),str(min([int(i) for i in numbers.split(" ")])))
+
+
+high_and_low("1 2 3 4 5")  # return "5 1"
