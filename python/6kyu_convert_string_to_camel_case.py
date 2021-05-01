eb874a63ea2be62bc4d9dc0c8d3b73e3a840ce21
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-05-01 08:10:40 +0200

    mesasge

--- python/6kyu_convert_string_to_camel_case.py
+++ python/6kyu_convert_string_to_camel_case.py
@@ -10,3 +10,14 @@ assert to_camel_case("the_stealth_warrior") == "theStealthWarrior"
 assert to_camel_case("The-Stealth-Warrior") =="TheStealthWarrior"
 assert to_camel_case("A-B-C") == "ABC"
 
+# https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+
+def find_it(seq):
+    arr_res = [seq.count(item)for item in seq]
+    for item in arr_res:
+        if item % 2 != 0:
+            return(seq[arr_res.index(item)])
+            
+
+
+assert find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) == 5
