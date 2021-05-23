01d4dc712c5a2ca34a447b660108757c84a1a472
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-05-23 20:07:09 +0200

    message

--- javascript/8kyu_convert_bolean_values_to_string.js
+++ javascript/8kyu_convert_bolean_values_to_string.js
@@ -28,3 +28,30 @@ function sumStr_1(a,b) {
 }
 
 sumStr_1("","5")
+
+// https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/javascript
+
+function firstNonConsecutive (arr) {
+  var min = Math.min(...arr)
+  var max = Math.max(...arr)
+  arr_n = []
+  for (var i = min; i <= max; i ++){
+    arr_n.push(i)
+  }
+  for (var i = 0; i <= arr_n.length; i ++){
+    if (arr_n[i] != arr[i]) return (arr[i])
+  }
+    return null
+}
+
+console.log(firstNonConsecutive([ 1, 2, 3, 4 ]))
+
+// better solution 
+function firstNonConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; ++i) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i + 1]
+    }
+  }
+  return null
+}
