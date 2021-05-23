6ff330ee06b14b201f94e0c20280f45ba3a69a6d
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-05-23 13:59:29 +0200

    message

--- javascript/8kyu_convert_bolean_values_to_string.js
+++ javascript/8kyu_convert_bolean_values_to_string.js
@@ -8,3 +8,23 @@ function boolToWord( bool ){
   }
 
 assert.equal(boolToWord(true),'Yes')
+
+// https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+function sumStr(a,b) {
+  if (a === "") {
+    a = 0
+  }
+  if (b === ""){
+    b = 0
+  }
+  return (parseInt(a)+parseInt(b)).toString() 
+}
+
+// better solution 
+function sumStr_1(a,b) {
+  const num1 = Number(a);
+  const num2 = Number(b);
+  return String(num1 + num2)
+}
+
+sumStr_1("","5")
