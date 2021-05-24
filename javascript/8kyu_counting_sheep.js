6cb2f83db9adabbeee1146ebb65c9d01c0c3735c
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-05-24 08:43:41 +0200

    message

--- javascript/8kyu_counting_sheep.js
+++ javascript/8kyu_counting_sheep.js
@@ -25,3 +25,16 @@ assert.equal(countSheeps(array1),17)
 function countSheeps(arrayOfSheeps) {
     return arrayOfSheeps.filter(Boolean).length;
   }
+
+function find_average(array) {
+  var sum = array.reduce(function(total, item){
+  return total +item
+  },0)
+  return array.length > 0 ? sum/array.length : 0
+}
+
+// better solution 
+function find_average(array) {
+  var sum = array.reduce((a, b) => a + b, 0);
+  return sum/array.length;
+}
