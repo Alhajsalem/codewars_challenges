b495a5ab75c7eb80ca3ef5affe9d528ba064a7bb
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-05-24 09:10:24 +0200

    message

--- javascript/8kyu_counting_sheep.js
+++ javascript/8kyu_counting_sheep.js
@@ -38,3 +38,25 @@ function find_average(array) {
   var sum = array.reduce((a, b) => a + b, 0);
   return sum/array.length;
 }
+
+// https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/javascript
+function findOutlier(integers){
+  odd_array = []
+  even_array = []
+  integers.forEach(function(item){
+    if (item % 2 == 0){
+      even_array.push(item)
+    }
+    else if (item % 2 != 0){
+      odd_array.push(item)
+    }
+  })
+  return odd_array.length == 1 ? odd_array[0] : even_array[0]
+}
+
+// better solution
+function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}
