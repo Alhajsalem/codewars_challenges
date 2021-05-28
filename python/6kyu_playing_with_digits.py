ff22dd4c2b87ee14194e5501aea145113a5060ee
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-05-28 22:25:22 +0200

    message

--- python/6kyu_playing_with_digits.py
+++ python/6kyu_playing_with_digits.py
@@ -11,3 +11,12 @@ assert (dig_pow(46288, 5)) ==  -1
 def dig_pow(n, p):
     k, fail = divmod(sum(int(d)**(p + i) for i, d in enumerate(str(n))), n)
     return -1 if fail else k
+
+# https://www.codewars.com/kata/5848565e273af816fb000449/train/python
+
+import re
+def encrypt_this(text):
+    return re.sub(r'\b(\w)(\w?)(\w*?)(\w?)\b', lambda m: '{}'.format(str(ord(m.group(1))) + m.group(4) + m.group(3) + m.group(2)), text).replace('   ', ' ').replace('  ', ' ')
+
+
+print(encrypt_this("A wise old owl lived in an oak"))
