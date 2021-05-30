bab10834d76fdc9d2e291d9345adb49bcd9b202b
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-05-30 16:37:03 +0200

    message

--- python/5kyu_pick_peaks.py
+++ python/5kyu_pick_peaks.py
@@ -13,3 +13,39 @@ def pick_peaks(arr):
 
 
 assert pick_peaks([1,2,3,6,4,1,2,3,2,1]) == {"pos":[3,7], "peaks":[6,3]}
+
+# https://www.codewars.com/kata/5ebcfe1b8904f400208e3f0d/train/python
+
+class FuncAdd:  
+    data = []
+    def __init__(self,func):
+        self.func = func
+        self.data.append(func)
+        setattr(FuncAdd,'data',self.data)
+        
+    def __call__(self,*a,**k):
+        if self.func not in self.data:
+            raise NameError
+        return tuple(i(*a,**k) for i in self.data if i.__name__==self.func.__name__) 
+    
+    @classmethod
+    def delete(cls,func_name):
+        cls.data = list(filter(lambda c: c.__name__!=func_name.func.__name__,cls.data))
+    
+    clear = classmethod(lambda cls: cls.data.clear())
+        
+
+
+@FuncAdd
+def foo():
+    return 'Hello'
+
+@FuncAdd
+def foo():
+    return 'World'
+
+print(foo()) #--> ('Hello', 'World')
+
+
+FuncAdd.delete(foo) # Delete all foo() functions only
+print(foo())
