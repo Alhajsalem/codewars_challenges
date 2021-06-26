708e7d9f8d7e4d0467747d3e3162c32398a6f86a
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-06-26 10:37:02 +0200

    message

--- shell/double_int.sh
+++ shell/double_int.sh
@@ -3,9 +3,19 @@ num1=$1
 ans=$((num1 * 2))
 echo $ans
 
-
+#############
 #!/bin/bash
 greet() {
     echo "hello world!";
 }
 greet 
+
+##############
+n=$1
+start=$((n*(n-1)+1))
+end=$((start+2*n-1))
+sum=0
+for ((i=start;i<=end;i=i+2)); do
+    sum=$((sum+i))
+done
+echo $sum
