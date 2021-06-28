1db73fcbb63f8e532f649cafd533019cec019fcf
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-06-28 08:27:42 +0200

    message

--- shell/divided_by.sh
+++ shell/divided_by.sh
@@ -7,3 +7,30 @@ then
 else
     echo "false"
 fi
+
+
+#!/bin/bash
+# input : 2 strings with substrings separated by `,`
+# output: number as a string
+accum () {
+    ARRAY=()
+    for i in $(echo $1 | sed "s/,/ /g")
+        do
+        for ii in $(echo $2 | sed "s/,/ /g")
+            do
+            x=$(( ${#ii}-${#i} ))
+            y=$( sed "s/-//" <<< $x )
+            ARRAY+=($y) 
+        done
+    done
+    if [ "${#ARRAY[@]}" -eq 0 ]; then
+        echo -1 
+    fi
+    max=0
+    for v in "${ARRAY[@]}"; do
+    if (( $v > $max )); then max=$v; fi; 
+    done
+    echo $max
+
+}
+accum hoqq,bbllkw,oox,ejjuyyy,plmiis,xxxzgpsssa,xxwwkktt,znnnnfqknaz,qqquuhii,dvvvwz cccooommaaqqoxii,gggqaffhhh,tttoowwwmmww
