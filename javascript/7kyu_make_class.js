b1bc7080b78778363df86e3708ed94cca9ed6d7f
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-07-04 10:29:12 +0200

    message

--- javascript/7kyu_make_class.js
+++ javascript/7kyu_make_class.js
@@ -7,11 +7,10 @@ function makeClass(...params) {
   }
 
 function makeClass(...properties) {
-  return function (...n) {
+  return function (...n){
     properties.forEach((x, y) => (this[x] = n[y]));
-  };
+  }; 
 }
 
 const Animel = makeClass('name','species','age','health','weight','color')
 const dog2 = new Animel("Bob", "Dog", "5", "good", "50lb", "brown");
-console.log(dog2)
