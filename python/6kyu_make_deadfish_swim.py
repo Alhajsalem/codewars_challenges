6b03af70ddcd741e8a76ccc90922208248d02be6
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-07-31 18:29:35 +0200

    message

--- python/6kyu_make_deadfish_swim.py
+++ python/6kyu_make_deadfish_swim.py
@@ -23,3 +23,17 @@ def parse(data):
     for a in data:
         v = action[a](v, r) if a in action else v
     return r
+
+from collections import Counter
+def letter_count(s):
+    return Counter(s)
+print(letter_count('arithmetics')) #=> {"a": 1, "c": 1, "e": 1, "h": 1, "i": 2, "m": 1, "r": 1, "s": 1, "t": 2}
+
+def up_array(arr):
+    if not arr or min(arr) < 0 or max(arr) > 9:
+        return None
+    else:
+        return [int(y) for y in str(int("".join([str(x) for x in arr])) + 1)]
+
+
+up_array([])#, [2,4,0]
