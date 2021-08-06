d99b0b2e0070f48b0cbac6b2814348b9ebd05989
Yousef Alhajsalem <Alhajsalem@gmail.com>
2021-08-06 18:21:07 +0200

    messaege

--- javascript/7kyu_two_or_one.js
+++ javascript/7kyu_two_or_one.js
@@ -20,3 +20,38 @@ function toWeirdCase(string){
 }
 
 toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
+
+function removeEveryOther(arr){
+   return arr.filter((item,index) => index %2 == 0)
+}
+removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])//,['Hello', 'Hello Again']);
+
+function solution(nums){
+  return (nums || []).sort((a,b)=> a-b)
+}
+solution([1,2,3,10,5])//, [1,2,3,5,10])
+solution(null)//, []))
+
+function nameShuffler(str){
+  return str.split(" ").reverse().join(" ")
+}
+nameShuffler('john McClane')//; => "McClane john"
+
+// Sum Numbers
+function sum (numbers) {
+  return numbers.reduce((a, b) => a + b, 0)
+};
+
+console.log(sum([]))//, 0;
+console.log(sum([1, 5.2, 4, 0, -1]))//, 9.2;
+
+var breakChocolate = function(n, m) {
+  return (n*m === 0) ? 0 : n * m - 1;
+ };
+
+ function removeUrlAnchor(url){
+  
+  return url.split("#")[0]
+}
+
+removeUrlAnchor('www.codewars.com#about')//, 'www.codewars.com')
