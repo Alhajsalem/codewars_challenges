e1a2715410c627aa06f160b1756ebb0f72af50d5
Yousef Alhajsalem <Yousef@Yousefs-MacBook-Pro.local>
2022-01-09 14:39:57 +0100

    new challenges

--- python/7kyu_is_integer_array.py
+++ python/7kyu_is_integer_array.py
@@ -1,49 +1,49 @@
-# def is_int_array(a):
-#     return isinstance(a, list) and all(isinstance(x, (int, float)) and x == int(x) for x in a)
+def is_int_array(a):
+    return isinstance(a, list) and all(isinstance(x, (int, float)) and x == int(x) for x in a)
 
-# is_int_array([1, 2, None])
+is_int_array([1, 2, None])
 
-# def pair_zeros(arr):
-#     lst = []
-#     flag = False
-#     for i in arr:
-#         if i == 0:
-#             if not flag:
-#                 lst.append(i)
-#             flag = not flag
-#         else:
-#             lst.append(i)
-#     return lst
-
-
-# pair_zeros([1,0,1,0,2,0,0,3,0])#,[1,0,1,2,0,3,0]
-
-# def sum_digits(number):
-#     return sum([int(i) for i in str(abs(number))])
+def pair_zeros(arr):
+    lst = []
+    flag = False
+    for i in arr:
+        if i == 0:
+            if not flag:
+                lst.append(i)
+            flag = not flag
+        else:
+            lst.append(i)
+    return lst
 
 
-# print(sum_digits(-32))#, 5
+pair_zeros([1,0,1,0,2,0,0,3,0])#,[1,0,1,2,0,3,0]
 
-# def incrementer(nums):
-#    print([ int(str((i+1+j))[-1:])  for i, j in enumerate(nums)])
+def sum_digits(number):
+    return sum([int(i) for i in str(abs(number))])
 
-# incrementer([4, 6, 7, 1, 3])#, [5, 8, 0, 5, 8])
 
-# def duplicate_elements(m, n):
-#     for i in m:
-#         if i in n:
-#             return True
-#     return False
+print(sum_digits(-32))#, 5
 
-# def next_smaller(n):
-#     countdown = n-1
-#     while countdown > 9:
-#         if "".join(sorted(str(countdown))) == "".join(sorted(str(n))):
-#             return countdown
-#         countdown = countdown - 1
-#     return -1
+def incrementer(nums):
+   print([ int(str((i+1+j))[-1:])  for i, j in enumerate(nums)])
+
+incrementer([4, 6, 7, 1, 3])#, [5, 8, 0, 5, 8])
+
+def duplicate_elements(m, n):
+    for i in m:
+        if i in n:
+            return True
+    return False
+
+def next_smaller(n):
+    countdown = n-1
+    while countdown > 9:
+        if "".join(sorted(str(countdown))) == "".join(sorted(str(n))):
+            return countdown
+        countdown = countdown - 1
+    return -1
     
-# print(next_smaller(123456789))#, 790
+print(next_smaller(123456789))#, 790
 
 def longest_slide_down(pyramid):
     if len(pyramid) == 1:
