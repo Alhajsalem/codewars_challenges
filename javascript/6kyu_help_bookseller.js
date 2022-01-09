3954d33423ec89ef16ba301877c046dbc262f386
Yousef Alhajsalem <Yousef@Yousefs-MacBook-Pro.local>
2022-01-09 18:55:23 +0100

    adding new chaellenge

--- javascript/6kyu_help_bookseller.js
+++ javascript/6kyu_help_bookseller.js
@@ -1,18 +1,44 @@
-function f(x, cc) { 
-    const keys = Object.keys(cc);
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];  
-      if (cc[key] == x){
-        return(cc[keys[(i+1) % keys.length]]);
-      }
+function f(x, cc) {
+  const keys = Object.keys(cc);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    if (cc[key] == x) {
+      return cc[keys[(i + 1) % keys.length]];
     }
   }
-f( 3, { a:3, b:4, c:5 } )//, 4 
+}
+f(3, { a: 3, b: 4, c: 5 }); //, 4
 
 // better solution
-function f(x, obj) { 
-    const arr = Object.values(obj)
-    let ind = arr.indexOf(x)
-    ind++
-    return arr[ind % 3]
+function f(x, obj) {
+  const arr = Object.values(obj);
+  let ind = arr.indexOf(x);
+  ind++;
+  return arr[ind % 3];
+}
+
+function singleDigit(n) {
+  if (n < 10) return n;
+  sum = n
+    .toString(2)
+    .toString()
+    .split("")
+    .map(Number)
+    .reduce(function (a, b) {
+      return a + b;
+    }, 0);
+  console.log(sum);
+  if (sum >= 10) {
+    return singleDigit(sum);
+  } else {
+    return sum;
   }
+}
+// better solution
+function singleDigit(n) {
+  while (n >= 10) {
+    n = (n.toString(2).match(/1/g) || []).length;
+  }
+  return n;
+}
+console.log(singleDigit(443566));
