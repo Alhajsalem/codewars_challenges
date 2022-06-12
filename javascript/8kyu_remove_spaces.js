32a71e2b20e4e9ef1b8fe92c2efc09e08dd5d5d6
Yousef Alhajsalem <alhajsalem@gmail.com>
2022-06-12 08:02:50 +0200

    adding new challenges

--- javascript/8kyu_remove_spaces.js
+++ javascript/8kyu_remove_spaces.js
@@ -1,3 +1,8 @@
 const result =  (x) => x.replace(/\s/g, '');
+const sayHello = (name, city, state ) => `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+const binToDec = (bin) =>  parseInt( bin, 2 );
+console.log(sayHello(['John', 'Smith', 'martin'], 'Phoenix', 'Arizona'));
+console.log(result('8 j 8   mBliB8g  imjB8B8  jl  B'))
+console.log(binToDec("1001001"))
 
-console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
+
